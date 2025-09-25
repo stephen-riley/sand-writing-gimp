b9ff75801f09fb304f6ec4193d805dd32c47343d
Stephen Riley <jstephen.riley@yahoo.com>
2025-09-25 15:42:46 -0700

    initial check-in

---
+++ hybrid/hybrid layered old.py
@@ -0,0 +1,172 @@
+def dir_pretty(obj): print(*dir(obj), sep='\n')
+
+def props_pretty(filter):
+	props = [p.get_name() for p in filter.list_properties()]
+	print(*props, sep='\n')
+
+def copy_layer(image, layer, name, order=0):
+	new_layer = layer.copy()
+	new_layer.set_name(name)
+	image.insert_layer(new_layer, None, order)
+	return new_layer
+
+def spread(layer, spread):
+	f = Gimp.DrawableFilter.new(layer, "gegl:noise-spread", "")
+	c = f.get_config()
+	c.set_property("amount-x", spread)
+	c.set_property("amount-y", spread)
+	f.update()
+	layer.merge_filter(f)
+	layer.resize_to_image_size()
+	return layer
+
+def minmax_newton_low(drawable, epsilon = 0.001):
+	low = 0
+	high = 255
+	threshold = 1.0 - epsilon
+	count = 0
+	while True:
+		count = count + 1
+		if count == 16: break
+		pt = int((low + high) / 2)
+		_, _, _, _, _, _, percentile = drawable.histogram(Gimp.HistogramChannel.VALUE, pt / 255.0, 1.0)
+		if percentile >= threshold:
+			new_low = pt
+			new_high = high
+		else:
+			new_low = low
+			new_high = pt
+		# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high}")
+		if low == new_low and high == new_high: break
+		low = new_low
+		high = new_high
+	return low - 1
+
+def minmax_newton_high(drawable, start_low, epsilon = 0.001):
+	low_norm = start_low / 255.0
+	low = start_low
+	high = 255
+	threshold = 1.0 - epsilon
+	_, _, _, _, _, _, volume = drawable.histogram(Gimp.HistogramChannel.VALUE, low_norm, 1.0)
+	volume = volume * threshold
+	count = 0
+	while True:
+		count = count + 1
+		if count == 16: break
+		pt = int((low + high) / 2)
+		_, _, _, _, _, _, percentile = drawable.histogram(Gimp.HistogramChannel.VALUE, low_norm, high / 255.0)
+		if percentile > threshold:
+			new_low = low
+			new_high = pt
+		elif percentile < threshold:
+			new_low = pt
+			new_high = high
+		else:
+			# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high} ***")
+			break
+		# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high}")
+		if low == new_low and high == new_high: break
+		low = new_low
+		high = new_high
+	return high + 1
+
+def minmax(drawable, epsilon = 0.001):
+	low = minmax_newton_low(drawable, epsilon)
+	high = minmax_newton_high(drawable, low, epsilon)
+	return low, high
+
+def alpha_to_selection(image, layer):
+	image.select_item(Gimp.ChannelOps.REPLACE, layer)
+
+def shift_levels_gegl(layer, in_low, in_high, out_low, out_high):
+	# assumes layer is only white pixels or transparent
+	f = Gimp.DrawableFilter.new(layer, "gegl:levels", "")
+	c = f.get_config()
+	# c.set_property('gamma', 1.0)
+	c.set_property('in-low', in_low / 255.0)
+	c.set_property('in-high', in_high / 255.0)
+	c.set_property('out-low', out_low / 255.0)
+	c.set_property('out-high', out_high / 255.0)
+	f.update()
+	layer.append_filter(f)
+	layer.merge_filters()
+
+img = Gimp.get_images()[0]
+sand_layer = img.get_layer_by_name('sand')
+text_layer = img.get_layer_by_name('text')
+
+# constants / args
+base_v = 100
+nearest = 5
+indent_offset_v = 0
+spread_layer_count = 8
+spread_incr = 3
+rill_offset_v = 5
+rill_step_offset_v = 3
+outline_growth_factor = 8
+outline_shrink_factor = 3
+final_base_v = 75
+
+# change image encoding to linear
+img.convert_precision(Gimp.Precision.U8_LINEAR)
+
+# get range
+low_v, high_v = minmax(sand_layer)
+v_range = nearest * int((high_v - low_v) / nearest)
+
+# shift sand layer down to base_v of 100
+shift_levels_gegl(sand_layer, low_v, high_v, base_v, base_v + v_range)
+
+# make dedent layer 95 "indent - 95"
+text_layer.invert(False)
+indent_v = int(base_v + v_range / 2 + indent_offset_v)
+indent_layer = copy_layer(img, text_layer, f"indent - {indent_v}")
+shift_levels_gegl(indent_layer, 255, 255, indent_v, indent_v)
+
+# make white outline layer (8px grow, 8px shrink)
+outline_layer = copy_layer(img, text_layer, "outline")
+alpha_to_selection(img, text_layer)
+Gimp.Selection.grow(img, outline_growth_factor)
+Gimp.context_set_foreground(Gegl.Color.new("white"))
+outline_layer.edit_fill(0)
+alpha_to_selection(img, text_layer)
+Gimp.Selection.shrink(img, outline_shrink_factor)
+outline_layer.edit_clear()
+Gimp.Selection.none(img)
+
+top_base_v = base_v + v_range
+spread_layers = []
+for n in range(1, spread_layer_count + 1):
+	v = top_base_v + rill_offset_v + (spread_layer_count + 1 - n) * rill_step_offset_v
+	spread_dist = spread_incr * n
+	l = copy_layer(img, outline_layer, f"spread - {spread_dist} @ {v}")
+	shift_levels_gegl(l, 255, 255, v, v)
+	spread(l, spread_dist)
+	spread_layers.append(l)
+
+outline_layer.set_visible(False)
+text_layer.set_visible(False)
+sand_layer = img.get_layer_by_name('sand')
+new_low_v, new_high_v = minmax(sand_layer)
+
+# signature
+signature = "afiril"
+font_size = 72
+text_vertical_offset = 100
+font = Gimp.Font.get_by_name('Sans-serif Bold Italic')
+hex_color = format(indent_v, 'x')
+Gimp.context_set_foreground(Gegl.Color.new(f"#{hex_color}{hex_color}{hex_color}ff"))
+extents = Gimp.text_get_extents_font(signature, font_size, font)
+x = (img.get_width() - extents.width) / 2
+y = img.get_height() - text_vertical_offset - extents.height
+signature_layer = Gimp.text_font(img, None, x, y, signature, -1, True, font_size, font)
+img.merge_down(signature_layer, 0)
+
+img.convert_precision(Gimp.Precision.U8_NON_LINEAR)
+img.merge_visible_layers(Gimp.MergeType.EXPAND_AS_NECESSARY)
+
+sand_layer = img.get_layer_by_name('sand')
+new_low_v, new_high_v = minmax(sand_layer)
+shift_levels_gegl(sand_layer, new_low_v, new_high_v, final_base_v, final_base_v + (new_high_v - new_low_v + 1))
+
+print('Done.')
---
+++ hybrid/hybrid.py
@@ -0,0 +1,184 @@
+import array
+import gi
+from gi.repository import Gimp
+
+def dir_pretty(obj): print(*dir(obj), sep='\n')
+
+def props_pretty(filter):
+	props = [p.get_name() for p in filter.list_properties()]
+	print(*props, sep='\n')
+
+def copy_layer(image, layer, name, order=0):
+	new_layer = layer.copy()
+	new_layer.set_name(name)
+	image.insert_layer(new_layer, None, order)
+	return new_layer
+
+def spread(layer, spread):
+	f = Gimp.DrawableFilter.new(layer, "gegl:noise-spread", "")
+	c = f.get_config()
+	c.set_property("amount-x", spread)
+	c.set_property("amount-y", spread)
+	f.update()
+	layer.merge_filter(f)
+	layer.resize_to_image_size()
+	return layer
+
+def apply_bump_map(base_layer, map_layer, invert = True):
+	bumpmap_filter = Gimp.DrawableFilter.new(base_layer, "gegl:bump-map", "")
+	bumpmap_filter.set_aux_input('aux', map_layer)
+	bumpmap_filter_config = bumpmap_filter.get_config()
+	bumpmap_filter_config.set_property("type", "linear")
+	bumpmap_filter_config.set_property("compensate", True)
+	bumpmap_filter_config.set_property("invert", invert)
+	bumpmap_filter_config.set_property("azimuth", 135.0)
+	bumpmap_filter_config.set_property("elevation", 45.0)
+	bumpmap_filter_config.set_property("depth", 3)
+	bumpmap_filter_config.set_property("offset-x", 0)
+	bumpmap_filter_config.set_property("offset-y", 0)
+	bumpmap_filter_config.set_property("waterlevel", 1.0)
+	bumpmap_filter_config.set_property("ambient", 0.39)
+	bumpmap_filter.update()
+	base_layer.merge_filter(bumpmap_filter)
+	return base_layer
+
+def chisel(image, layer):
+	procedure = Gimp.get_pdb().lookup_procedure('script-fu-chisel-300')
+	config = procedure.create_config()
+	config.set_property('run-mode', Gimp.RunMode.NONINTERACTIVE)
+	config.set_property('otherImage', image)
+	config.set_property('drawable', layer)
+	config.set_property('option', 0)			# bump curve (Linear | Spherical | Sinusoidal)
+	config.set_property('adjustment', 20)		# bevel width
+	config.set_property('adjustment-2', 10)		# bevel softness
+	config.set_property('adjustment-3', 1.0)	# bevel roundess
+	config.set_property('adjustment-4', 0)		# bevel power
+	config.set_property('adjustment-5', 135)	# azimuth
+	config.set_property('adjustment-6', 20.0)	# elevation
+	config.set_property('adjustment-7', 20)		# depth
+	config.set_property('option-2', 0) 			# mode (Chisel-off Edges | Carve-in)
+	config.set_property('option-3', 0)			# Location (Inside | Outside)
+	config.set_property('adjustment-8', 0)		# post-effect blur
+	config.set_property('toggle', False)		# keep bump map
+	procedure.run(config)
+	return image.get_layer_by_name('text bevel')
+
+def minmax_newton_low(drawable, epsilon = 0.001):
+	low = 0
+	high = 255
+	threshold = 1.0 - epsilon
+	count = 0
+	while True:
+		count = count + 1
+		if count == 16: break
+		pt = int((low + high) / 2)
+		_, _, _, _, _, _, percentile = drawable.histogram(Gimp.HistogramChannel.VALUE, pt / 255.0, 1.0)
+		if percentile >= threshold:
+			new_low = pt
+			new_high = high
+		else:
+			new_low = low
+			new_high = pt
+		# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high}")
+		if low == new_low and high == new_high: break
+		low = new_low
+		high = new_high
+	return low - 1
+
+def minmax_newton_high(drawable, start_low, epsilon = 0.001):
+	low_norm = start_low / 255.0
+	low = start_low
+	high = 255
+	threshold = 1.0 - epsilon
+	_, _, _, _, _, _, volume = drawable.histogram(Gimp.HistogramChannel.VALUE, low_norm, 1.0)
+	volume = volume * threshold
+	count = 0
+	while True:
+		count = count + 1
+		if count == 16: break
+		pt = int((low + high) / 2)
+		_, _, _, _, _, _, percentile = drawable.histogram(Gimp.HistogramChannel.VALUE, low_norm, high / 255.0)
+		if percentile > threshold:
+			new_low = low
+			new_high = pt
+		elif percentile < threshold:
+			new_low = pt
+			new_high = high
+		else:
+			# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high} ***")
+			break
+		# print(f"{low}-{high} {pt} ({percentile}) {new_low}-{new_high}")
+		if low == new_low and high == new_high: break
+		low = new_low
+		high = new_high
+	return high + 1
+
+def minmax(drawable, epsilon = 0.001):
+	low = minmax_newton_low(drawable, epsilon)
+	high = minmax_newton_high(drawable, low, epsilon)
+	return low, high
+
+def set_text_background_color(image, sand, text, low_value, ratio=0.95):
+	image.select_item(Gimp.ChannelOps.REPLACE, text)
+	sand.edit_clear()
+	norm_lo = low_value / 255.0
+	hex = format(int(low_value * ratio), 'x')
+	color = Gegl.Color.new(f"#{hex}{hex}{hex}ff")
+	Gimp.context_set_foreground(color)
+	sand.edit_fill(0)
+	Gimp.Selection.none(image)
+
+def alpha_to_selection(image, layer):
+	image.select_item(Gimp.ChannelOps.REPLACE, layer)
+
+img = Gimp.get_images()[0]
+sand_layer = img.get_layer_by_name('sand')
+text_layer = img.get_layer_by_name('text')
+
+low_value, high_value = minmax(sand_layer)
+
+set_text_background_color(img, sand_layer, text_layer, low_value)
+
+text_layer.invert(False)
+
+spread_layers = [spread(copy_layer(img, text_layer, f"text - {dist}"), dist) for dist in [10, 20, 20, 30, 40, 50]]
+
+img.select_item(Gimp.ChannelOps.REPLACE, text_layer)
+for l in spread_layers:
+	l.edit_clear()
+
+Gimp.Selection.none(img)
+
+for l in spread_layers:
+	apply_bump_map(sand_layer, l)
+
+Gimp.displays_flush()
+
+text_layer.set_visible(False)
+img.merge_visible_layers(0)
+
+sand_layer = img.get_layer_by_name('sand')
+
+l, h = minmax(sand_layer)
+
+l = l / 255.0
+h = h / 255.0
+width = h - l
+offset = l * 0.1
+
+Gimp.Drawable.levels(
+	sand_layer,
+	Gimp.HistogramChannel.VALUE,
+	l * 0.9,		# bracket low end of visible
+	h,				# bracket high end of visible
+	False,			# don't clamp
+	1.0,			# gamma
+	offset,			# start of output range
+	width + offset,	# top of output range
+	False)			# clamp output
+
+
+levels = [42/255.0 if i > 128 else i/128.0 * 42/255.0 for i in range(0,256)]
+sand_layer.curves_explicit(Gimp.HistogramChannel.VALUE, levels)
+
+print("Done.")
